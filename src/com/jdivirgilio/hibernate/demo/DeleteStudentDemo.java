6552396658a0c06acc9b8b080cde9ddb5a670647
John DiVirgilio <lurkingmenace@gmail.com>
2018-08-13 17:12:52 -0500

    Delete completed

---
+++ src/com/jdivirgilio/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,63 @@
+package com.jdivirgilio.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.jdivirgilio.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// Create Session Factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml") // Default name of file. Not necessary to include here.
+																// Must be in class path though!
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// Create Session
+		Session session = factory.getCurrentSession();
+	
+		try {
+			// Session can only be used on one item.
+			Student newStudent = new Student("John", "DiVirgilio", "anyone@anywhere.com");
+			session.beginTransaction();
+			session.save(newStudent);
+			session.getTransaction().commit();
+			
+			session = factory.getCurrentSession();
+			newStudent = new Student("Mary", "Jane", "applebottom@anywhere.com");
+			session.beginTransaction();
+			session.save(newStudent);
+			session.getTransaction().commit();
+
+			session = factory.getCurrentSession();
+			newStudent = new Student("Apple", "Bottom", "applebottom@anywhere.com");
+			session.beginTransaction();
+			session.save(newStudent);
+			session.getTransaction().commit();
+			
+			// Delete
+						
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			newStudent = session.get(Student.class, newStudent.getId());
+			System.out.println("deleting " + newStudent);
+			session.delete(newStudent);
+			session.getTransaction().commit();
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			session.createQuery("delete from Student where id=2").executeUpdate();
+			session.getTransaction().commit();
+
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
